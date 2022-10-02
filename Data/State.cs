b04fa5aa692c5d90b82bb05d6346252e4fdbf360
Rio Cadeliña <cadelina13@gmail.com>
2022-10-02 22:00:01 +0800

    Add project files.

---
+++ Data/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROFILE_WEBSITE_MAUI.Data
+{
+    internal class State
+    {
+        public static string APPBAR_TITLE { get; set; }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PROFILE_WEBSITE_MAUI;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
